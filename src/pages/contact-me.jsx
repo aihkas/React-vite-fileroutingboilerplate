9947edb6bc2ccdfcf287f772feb9fbd0233b2efe
Aiham Alkaseer <ayhamk@live.com>
2022-10-15 17:02:27 +0200

    fix

--- src/pages/contact-me.jsx
+++ src/pages/contact-me.jsx
@@ -9,7 +9,7 @@ const ContactMe = () => {
         title="Contact me"
       />
       <MainBody>
-          <div className="flex flex-col justify-center items-start space-y-5">
+          <div className="flex  flex-col justify-center items-start space-y-5">
                <p>Send me a Mail:</p>
         <a href="mailto:email@gmail.com" className="text-link-color hover:text-link-color/80">Mail me!</a>
           </div>
