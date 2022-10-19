7f88c76baa75d7d63ed7a82febb000374b4244a9
Aiham Alkaseer <ayhamk@live.com>
2022-10-19 17:56:57 +0200

    Use vite svg plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr';
+
 const { plugin: mdPlugin, Mode } = require('vite-plugin-markdown')
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mdPlugin({ mode: [Mode.HTML, Mode.TOC, Mode.REACT] })]
+    plugins: [react(), svgr(), mdPlugin({ mode: [Mode.HTML, Mode.TOC, Mode.REACT] })]
 })
